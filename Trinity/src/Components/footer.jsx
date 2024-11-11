30ed63f21c51197cc3d4d8ff73b51fff0fee10f0
sirisenju <amrohore4real@gmail.com>
2024-11-11 09:57:37 -0800

    modified committed

--- Trinity/src/Components/footer.jsx
+++ Trinity/src/Components/footer.jsx
@@ -2,18 +2,6 @@ import React from 'react'
 import Swipper from './swipper'
 
 function Footer() {
-
-    const data = [
-        {id: 1, content: `"I lift up my eyes to the hills-- where does my help come from? My help comes from the LORD, 
-                        the Maker of heaven and earth. indeed, he who watches over Israel will neither slumber nor 
-                        sleep. the sun will not harm you by day, nor the moon by night."`, verse: "Psalm 121 1"},
-        {id: 2, content: `"I lift up my eyes to the hills-- where does my help come from? My help comes from the LORD, 
-                        the Maker of heaven and earth. indeed, he who watches over Israel will neither slumber nor 
-                        sleep. the sun will not harm you by day, nor the moon by night."`, verse: "Psalm 121 1"},
-        {id: 2, content: `"I lift up my eyes to the hills-- where does my help come from? My help comes from the LORD, 
-                        the Maker of heaven and earth. indeed, he who watches over Israel will neither slumber nor 
-                        sleep. the sun will not harm you by day, nor the moon by night."`, verse: "Psalm 121 1"},
-    ]
     return (
         <footer className='min-h-[40vh] text-center bg-[#D3D3D3]'>
             <Swipper/>
